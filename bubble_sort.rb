120b60f7f77a4dc0263c35b3ae299b08ab90dcfd
VincenzoY <45444990+VincenzoY@users.noreply.github.com>
2019-11-05 14:40:36 -0700

    Create bubble_sort.rb
    Adding code for bubble sort. The amount of iterations isn't optimized, it just takes the array length and multiplies by 5

---
+++ bubble_sort.rb
@@ -0,0 +1,15 @@
+def bubble_sort(array)
+ iterations = 0
+ while iterations < (array.length*5)
+   array.each_with_index do |num, index|
+     if num.to_i > array[index+1].to_i
+       array[index, 2] = [array[index+1], array[index]]
+       break
+     end
+   end
+   iterations += 1
+ end
+ puts array.reject { |c| c.to_s.empty? }
+end
+ 
+bubble_sort([4,3,78,2,0,2,1,2,45,1,3,4])
